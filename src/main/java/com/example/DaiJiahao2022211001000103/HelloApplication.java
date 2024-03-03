5ccead4a33cc85e0d14f9ba671befe4bcf93934e
1 <3069684859@qq.com>
2024-03-03 16:40:11 +0800

    First Commit

---
+++ src/main/java/com/example/DaiJiahao2022211001000103/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.DaiJiahao2022211001000103;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
